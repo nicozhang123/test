709a6625b6ffc62196275d57f302a80dddede494
nicozhang123 <127396171+nicozhang123@users.noreply.github.com>
2023-05-23 11:48:07 +1200

    Add files via upload

---
+++ Batch/Launch.bat
@@ -0,0 +1,8 @@
+echo “start”
+echo “start client”
+start /B ./Client.exe
+echo “start server”
+start /B ./Server.exe
+echo “start cache”
+start /B ./Cache.exe
+echo “All the services has started......”
